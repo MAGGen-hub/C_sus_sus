77ecbcd7ca92e81188c9e417ae65eecd3eb7ca28
MAGGen-hub <isaykov2001@mail.ru>
2023-08-11 11:17:33 +0300

    Update env_pack.lua

--- env_pack.lua
+++ env_pack.lua
@@ -1,9 +1,20 @@
 a,b=cssc.load([[<A,E,dbg(p)>
 --Env setupper
 nE=->setmetatable({},{__index=_G});
+@G={}
+--GLOBAL: better strings
+G.strings==>
+    @sm=getmetatable("") --get strings metatable
+    sm.__autocomplete=sm.__index
+    sm.__index=s,i=>$sm.__autocomplete[i]||type(i)=="number"&&s:sub(i,i)||nil;
+    sm.__mod=s,f=> --self,format
+        /|type(f)==table?$s:format(unpack(f));
+        $:format(f);;
+            
+    
 
 --GLOBAL: __auto methamethod support
-cssc.enable__auto==>
+G.__autocomplete==>
     @f=fs.open("rom/apis/textutils.lua",'r')
     @t=f.readAll() f.close()
     @p='.-__)(.-x)(' --part of template
@@ -17,6 +28,9 @@ cssc.enable__auto==>
     /|b?error(b);
     $s,a
     ;
+    
+cssc.global=G
+
 --Vector pack .xyz - feature
 do
  @v=nE()
@@ -32,11 +46,25 @@ do
          /|#i==3?$v.new(s[f()],s[f()],s[f()]);--return vector
          ;)(s,i);
 end
---IO bitwise pack >> - read line << - write line
-do
 
+--custom read
+do
+ @c={gray=colors.gray,white=colors.white}
+ @br=load(("").dump(read),'cssc_read',nil,setmetatable({colors=c},{}))
+ debug.setupvalue(br,require'cc.expect'.expect)
+ env.read=_sReplaceChar,_tHistory,_fnComplete,_sDefault,_nTextColor,_nBackColor=>
+     c.gray=_nBackColor||colors.gray
+     c.white=_nTextColor||colors.white
+     $br(_sReplaceChar,_tHistory,_fnComplete,_sDefault); --colorised read autocompletion
 end
 
+--string pairs support
+
+--fields support
+
+--table copy clone size is_empty has_key, has_value
+
+cssc.env_version="1.2"
 ]],"CSSC_ENV",nil,cssc.__CSSC)
 b=b and error(b)
 a=a and a(...)
